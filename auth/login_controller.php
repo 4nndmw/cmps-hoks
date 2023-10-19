eee56fc71a77bedfe1fabb4bfaf76d7b5fa14ae5
4nndmw <anandamaulanaw1850@gmail.com>
2023-10-19 11:05:14 +0700

    update

---
+++ auth/login_controller.php
@@ -0,0 +1,51 @@
+ <?php
+
+    require_once "../config/Database.php";
+
+    class loginUser
+    {
+        private $connection;
+
+        public function __construct()
+        {
+            $this->connection = new Database;
+        }
+
+        public function roleUser()
+        {
+            if (isset($_POST['submit'])) {
+                $username = $_POST['uname'];
+                $password = $_POST['pass'];
+
+                $cekuser = mysqli_query($this->connection->conn, "SELECT * FROM user WHERE username = '$username' AND password = '$password'");
+
+                $hitung = mysqli_num_rows($cekuser);
+
+                if ($hitung) {
+                    $ambilData = mysqli_fetch_array($cekuser);
+                    $role = $ambilData['role'];
+
+                    if ($role == 'kaprodi') {
+                        // jika admin 
+                        $_SESSION['log'] = 'logged';
+                        $_SESSION['role'] = 'kaprodi';
+                        header('location:../views/kaprodi/tabel_judul_mahasiswa.php');
+                    } elseif ($role == "penguji") {
+                        $_SESSION['log'] = 'logged';
+                        $_SESSION['role'] = 'penguji';
+                        header('location:../views/dosenPenguji/tabel_nilai_sidang.php');
+                    } else {
+                        $_SESSION['log'] = 'logged';
+                        $_SESSION['role'] = 'mahasiswa';
+                        header('location:../views/mahasiswa/input_judul_mahasiswa.php');
+                    }
+                } else {
+                    echo "
+                    <script>
+                        alert('  ');
+                    </script>
+                    ";
+                }
+            }
+        }
+    }
